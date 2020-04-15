fc6a6bf38856468733f29201c7ca6fb00209003b
Kai SHEN <cylisery@outlook.com>
2020-04-15 21:19:25 +0900

    Create 0045-Jump_Game_II.py

---
+++ 0045-Jump_Game_II.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Solution(object):
+        
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        length = len(nums)
+
+        step = 0
+        farestPointCanJumpBeforeNeedJump = 0
+        needJumpPoint = 0
+
+        for i in xrange(length):
+            if i > needJumpPoint:
+                needJumpPoint = farestPointCanJumpBeforeNeedJump
+                step += 1
+
+            if needJumpPoint >= length-1:
+                return step
+
+            farestPointCanJumpBeforeNeedJump = max(farestPointCanJumpBeforeNeedJump, i+nums[i])
+
+        return step
